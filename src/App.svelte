2f9191c62e0a59db8e65a6218c737466ee40b7a5
MantasEdine <youcefmantas3@gmail.com>
2024-08-28 19:35:01 +0300

    data-binding/reactive-values

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,44 @@
 <script>
-let colour = "gold"
-let defaultColour = "Black";
-   
+   let Name = "Youcef";
+   let NickName = "Mantas";
+   $: fullName = `${Name} ${NickName}`;
+   $: {
+      console.log(Name);
+      console.log(NickName);
+
+   }
    const handleClick = () =>{
-    colour = defaultColour;
+      Name = NickName;
    }
-   const handleInput = (event) =>{
-   colour = event.target.value;
+   const handleChange = (e) =>{
+      Name = e.target.value;
    }
-</script>
-
-<main>
+   </script>
    
-   <h1 style="color: {colour};">Hello <strong>{colour}</strong></h1>
-   <button on:click={handleClick}>Click Me)</button>
-   <!--Two Way Data Binding-->
-   <!-- <input type="text" on:change={handleInput} value={colour}> -->
-   <input type="text" bind:value={colour}>
-     
-</main>
+   <main>
+    <div class="container">
+      <h1>Hello {fullName}</h1>
+      <button on:click={handleClick} style="color: {Name};">Click</button>
+      <span>By Clicking On Button You Will Know NickName!</span>
+      <input type="text" bind:value={Name} >
+      <input type="text" bind:value={NickName} >
 
-<style>
- 
-</style>
+
+    </div>     
+   </main>
+   
+   <style>
+      .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+      }
+   
+      h1, button, span, input {
+          margin: 0.5rem 0; /* Adds some space between the elements */
+      }
+      span {
+          color: antiquewhite;
+      }
+   </style>
+   
