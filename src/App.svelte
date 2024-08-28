bf3dedd407f89f0818c256bfca71a7b7379750fb
MantasEdine <youcefmantas3@gmail.com>
2024-08-28 16:52:28 +0300

    first-commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+let colour = "gold"
+let defaultColour = "Black";
+   
+   const handleClick = () =>{
+    colour = defaultColour;
+   }
+   const handleInput = (event) =>{
+   colour = event.target.value;
+   }
+</script>
+
+<main>
+   
+   <h1 style="color: {colour};">Hello <strong>{colour}</strong></h1>
+   <button on:click={handleClick}>Click Me)</button>
+   <!--Two Way Data Binding-->
+   <!-- <input type="text" on:change={handleInput} value={colour}> -->
+   <input type="text" bind:value={colour}>
+     
+</main>
+
+<style>
+ 
+</style>
