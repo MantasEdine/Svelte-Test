dc48a2fd6a84fe7bf7d3759615118735959f376b
MantasEdine <youcefmantas3@gmail.com>
2024-08-31 16:01:25 +0300

    conditionals/event-handlers

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,38 @@
 <script>
-   let Name = "Youcef";
-   let NickName = "Mantas";
-   $: fullName = `${Name} ${NickName}`;
-   $: {
-      console.log(Name);
-      console.log(NickName);
+ let people = [
+    { name: 'John', surname : 'Michael', age: 25, id : 1 },
+    { name: 'Karl', surname : 'Max', age: 26, id : 2 },
+    { name: 'Joseph', surname : 'Stalin', age: 27, id : 3 },
 
-   }
-   const handleClick = () =>{
-      Name = NickName;
-   }
-   const handleChange = (e) =>{
-      Name = e.target.value;
-   }
+]  
+    
+    const handleDelete = (id) =>{
+      people = people.filter(person =>{
+       return  person.id != id;
+      })
+     
+    }
+   
    </script>
    
    <main>
-    <div class="container">
-      <h1>Hello {fullName}</h1>
-      <button on:click={handleClick} style="color: {Name};">Click</button>
-      <span>By Clicking On Button You Will Know NickName!</span>
-      <input type="text" bind:value={Name} >
-      <input type="text" bind:value={NickName} >
-
-
-    </div>     
+    {#if people.length >= 3}
+     <div class="container">
+      <h1>People Infos :</h1>
+      {#each people as person (person.id) }
+      <h4>{person.name}</h4>
+      <span>{person.surname}</span>
+      <span>{person.age}</span>
+      <button on:click={()=> handleDelete(person.id)}>delete</button>
+      {:else}
+      <p>No people found</p>
+      {/each}
+      
+      
+     </div>   
+     {:else}  
+     <p>So Sad</p>
+     {/if}
    </main>
    
    <style>
@@ -34,7 +42,7 @@
           align-items: center;
       }
    
-      h1, button, span, input {
+      h1 {
           margin: 0.5rem 0; /* Adds some space between the elements */
       }
       span {
